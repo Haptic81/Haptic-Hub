f3106d8d6b1ebbcb0db8fe42bf9b03026220eb25
Hapticfr <eastin123yum@gmail.com>
2025-09-21 14:55:07 -0500

    Update intro.lua

--- intro.lua
+++ intro.lua
@@ -1,4 +1,4 @@
--- Made By Haptic 
+-- Made By Haptic
 
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
@@ -15,41 +15,41 @@ local gui = Instance.new("ScreenGui")
 gui.IgnoreGuiInset = true
 gui.Parent = guiParent
 
-local textLabel = Instance.new("TextLabel")
-textLabel.AnchorPoint = Vector2.new(0.5, 0.5)
-textLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
-textLabel.Size = UDim2.new(0, 300, 0, 40)
-textLabel.BackgroundTransparency = 1
-textLabel.Text = "HAPTIC-HUB"
-textLabel.TextColor3 = Color3.new(0, 0, 0)
-textLabel.Font = Enum.Font.GothamBold
-textLabel.TextScaled = true
-textLabel.RichText = true
-textLabel.TextTransparency = 1
-textLabel.Parent = gui
+local txt = Instance.new("TextLabel")
+txt.AnchorPoint = Vector2.new(0.5, 0.5)
+txt.Position = UDim2.new(0.5, 0, 0.5, 0)
+txt.Size = UDim2.new(0, 300, 0, 40)
+txt.BackgroundTransparency = 1
+txt.Text = "HAPTIC-HUB"
+txt.TextColor3 = Color3.new(0, 0, 0)
+txt.Font = Enum.Font.GothamBold
+txt.TextScaled = true
+txt.RichText = true
+txt.TextTransparency = 1
+txt.Parent = gui
 
 TweenService:Create(blur, TweenInfo.new(1), {Size = 20}):Play()
-TweenService:Create(textLabel, TweenInfo.new(1.5), {TextTransparency = 0}):Play()
+TweenService:Create(txt, TweenInfo.new(1.5), {TextTransparency = 0}):Play()
 task.wait(1.5)
 
-local text = textLabel.Text
-for i = 1, #text do
-    local formattedText = ""
-    for j = 1, #text do
-        local c = text:sub(j, j)
+local str = txt.Text
+for i = 1, #str do
+    local t = ""
+    for j = 1, #str do
+        local c = str:sub(j, j)
         if j <= i then
-            formattedText = formattedText .. "<font color=\"rgb(60,60,60)\">" .. c .. "</font>"
+            t = t .. "<font color=\"rgb(60,60,60)\">" .. c .. "</font>"
         else
-            formattedText = formattedText .. "<font color=\"rgb(0,0,0)\">" .. c .. "</font>"
+            t = t .. "<font color=\"rgb(0,0,0)\">" .. c .. "</font>"
         end
     end
-    textLabel.Text = formattedText
+    txt.Text = t
     task.wait(0.1)
 end
 
 task.wait(1)
 TweenService:Create(blur, TweenInfo.new(1), {Size = 0}):Play()
-TweenService:Create(textLabel, TweenInfo.new(1), {TextTransparency = 1}):Play()
+TweenService:Create(txt, TweenInfo.new(1), {TextTransparency = 1}):Play()
 task.wait(1)
 gui:Destroy()
 blur:Destroy()
